115af7132eab83e083385850d7e8627228e6f1e5
Zoee Silcock <mrzoee@gmail.com>
2015-09-19 13:43:25 +0200

    Initial commit

---
+++ Scripts/AddObjectToAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AddObjectToAsset {
+    [MenuItem("Assets/Add Object To Asset")]
+    private static void AddObject() {
+        Debug.Log("add object hype!");
+    }
+}
