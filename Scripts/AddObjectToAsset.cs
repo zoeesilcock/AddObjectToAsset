243335be9ce5f7fa3ad6a75d49c4e230f30898fd
Zoee Silcock <mrzoee@gmail.com>
2015-09-19 15:28:06 +0200

    Working solution with unfinished workflow

--- Scripts/AddObjectToAsset.cs
+++ Scripts/AddObjectToAsset.cs
@@ -2,9 +2,45 @@
 using UnityEditor;
 using System.Collections;
 
-public class AddObjectToAsset {
+public class AddObjectToAsset : EditorWindow {
+    public Object selectedObject;
+
     [MenuItem("Assets/Add Object To Asset")]
-    private static void AddObject() {
-        Debug.Log("add object hype!");
+    private static void ShowParentPicker() {
+        string childPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+        AddObjectToAsset window = GetWindow<AddObjectToAsset>();
+        window.selectedObject = AssetDatabase.LoadAssetAtPath(childPath, typeof(Object)) as Object;
+    }
+
+    void AddObjectTo(Object asset) {
+        Object objectCopy = Object.Instantiate(selectedObject);
+        objectCopy.name = selectedObject.name;
+
+        AssetDatabase.AddObjectToAsset(objectCopy, AssetDatabase.GetAssetPath(asset));
+        AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(selectedObject));
+        AssetDatabase.SaveAssets();
+    }
+
+    void ShowPicker() {
+        int controlID = EditorGUIUtility.GetControlID(FocusType.Passive);
+        EditorGUIUtility.ShowObjectPicker<Object>(null, false, "", controlID);
+    }
+
+    void OnGUI() {
+        string commandName = Event.current.commandName;
+
+        GUILayout.Box("Chose an asset which you would like this object to be a child of.");
+
+        if (GUILayout.Button("Pick a parent asset")) {
+            ShowPicker();
+        }
+
+        if (commandName == "ObjectSelectorClosed") {
+            Object selectedAsset = EditorGUIUtility.GetObjectPickerObject();
+            if (selectedAsset) {
+                AddObjectTo(selectedAsset);
+            }
+            Close();
+        }
     }
 }
